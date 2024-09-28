be2db13dfed779e4c4c828fa5e56dbf3d8c7391f
Zahangir Alam <zahangirdu1@gmail.com>
2024-09-28 16:15:19 +0600

    Added: Product Functionalities

---
+++ PSG.BusinessLogicLayer/ProductBusinessLayer.cs
@@ -0,0 +1,46 @@
+﻿using PSG.DataAccessLayer;
+using PSG.DataAccessLayer.Models;
+
+namespace PSG.BusinesLogicLayer
+{
+    public class ProductBusinessLayer
+    {
+        public bool Create(Product product)
+        {
+            ProductAdoDataAcessLayer  nextLayer = new ProductAdoDataAcessLayer();
+            bool result = nextLayer.Create(product);
+            return result;
+        }
+        
+        public bool Update(Product product)
+        {
+            ProductAdoDataAcessLayer nextLayer = new ProductAdoDataAcessLayer();
+            bool result = nextLayer.Update(product);
+            return result;
+        }
+        public Product GetProductById(int id)
+        {
+          ProductAdoDataAcessLayer nextLayer = new ProductAdoDataAcessLayer();
+          Product result = nextLayer.GetProductById(id);
+          return result;
+        }
+            
+        public bool DeleteById(int id) 
+        {
+            ProductAdoDataAcessLayer nextLayer = new ProductAdoDataAcessLayer();
+            bool result = nextLayer.DeleteById(id);
+            return result;
+        }
+
+        public List<Product> GetProducts()
+        {
+            ProductAdoDataAcessLayer nextLayer = new ProductAdoDataAcessLayer();
+            List<Product> products = nextLayer.GetProducts();
+            return products;
+        }  
+
+        
+
+       
+    }
+}
